53b4d63cd450050f9fa5b622095339863fe91455
Shahid Hasan Shuvo <mrshanshuvo@gmail.com>
2025-12-13 17:00:52 +0600

    applied data from the database in the templates

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+  post_title = models.CharField(max_length=60)
+  post_content = models.TextField()
+  published_data = models.DateTimeField( auto_now=True)
--- posts/views.py
+++ posts/views.py
@@ -1,44 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponseNotFound, Http404
-
-posts = [
-    {
-        'id': 1,
-        'title': 'How to Learn Python Fast',
-        'content': 'Start with the basics, practice regularly, and build small projects. Consistency is more important than speed.'
-    },
-    {
-        'id': 2,
-        'title': 'React vs Vue: Which One to Choose?',
-        'content': 'React is backed by Facebook and has a huge ecosystem. Vue is simpler to learn and offers great developer experience. Both are excellent choices depending on the project.'
-    },
-    {
-        'id': 3,
-        'title': 'Why Sleep Matters for Productivity',
-        'content': 'Lack of sleep reduces focus, creativity, and decision making. Sleeping 7-8 hours boosts mental performance and memory.'
-    },
-    {
-        'id': 4,
-        'title': 'Top 5 Tips for Job Interviews',
-        'content': 'Research the company, practice common questions, be confident, show your skills through examples, and follow up with a thank-you email.'
-    }
-]
-
+from django.http import Http404
+from .models import Post
+from django.shortcuts import get_list_or_404
 
 # Create your views here.
 def home(request):
-  return render(request, 'posts/index.html', {'posts' : posts})
+  all_posts = Post.objects.all()
+  return render(request, 'posts/index.html', {'posts' : all_posts})
 
 def post(request, id):
-  valid_id = False
-  for post in posts:
-    if post['id'] == id:
-      post_dict = post
-      valid_id = True
-      break
-  if valid_id:
-    return render(request, 'posts/post.html', {'post': post_dict})
-  else:
-    raise Http404()
+  post = get_list_or_404(Post,id=id)
+  return render(request, 'posts/post.html', {'post': post})
+
+  
+  
+
   
   
