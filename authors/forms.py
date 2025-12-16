2635cfd907c380274398c4c534a89db7caf828cf
Shahid Hasan Shuvo <mrshanshuvo@gmail.com>
2025-12-16 19:34:09 +0600

    learnt crud in modelform

--- authors/forms.py
+++ authors/forms.py
@@ -1,30 +1,35 @@
 from django import forms
 from .models import Author
+from django.core import validators
+
+
+def start_s(value):
+    if value[0] not in ["s", "S"]:
+        raise forms.ValidationError("Name should start with letter s/S")
 
 
 class AuthorsForm(forms.ModelForm):
+    name = forms.CharField(
+        validators=[validators.MaxLengthValidator(10), start_s],
+        widget=forms.TextInput(attrs={"class": "form-control"}),
+        label="Your Name",
+    )
+    email = forms.CharField(
+        validators=[validators.MaxLengthValidator(10), start_s],
+        widget=forms.TextInput(attrs={"class": "form-control"}),
+        label="Your Email",
+    )
+
     class Meta:
         model = Author
-        fields = ["name", "email", "contact_no", "bio"]
-
-        # custom labels (optional)
+        fields = "__all__"
         labels = {
-            "name": "Your Name",
-            "email": "Your Email",
             "contact_no": "Contact Number",
             "bio": "Your Details",
         }
-
-        # custom css (optional)
         widgets = {
-            "name": forms.TextInput(attrs={"class": "form-control"}),
-            "email": forms.TextInput(attrs={"class": "form-control"}),
             "contact_no": forms.TextInput(attrs={"class": "form-control"}),
             "bio": forms.Textarea(attrs={"class": "form-control"}),
         }
-
-        # customer help texts (optional)
         help_texts = {"email": "We only accept gmail"}
-
-        # customer error messages (optional)
         error_messages = {"name": {"required": "Name field is required"}}
