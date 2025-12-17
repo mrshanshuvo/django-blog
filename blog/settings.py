39af2f05915c3f53f458fc3f9afe63e3fb02e80d
Shahid Hasan Shuvo <mrshanshuvo@gmail.com>
2025-12-17 23:44:03 +0600

    learnt the session

--- blog/settings.py
+++ blog/settings.py
@@ -129,3 +129,5 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+SESSION_COOKIE_AGE = 5
--- sessiontut/urls.py
+++ sessiontut/urls.py
@@ -1,3 +1,8 @@
 from django.urls import path
+from sessiontut import views
 
-urlpatterns = []
+urlpatterns = [
+    path("set/", views.set),
+    path("get/", views.get),
+    path("delete/", views.delete),
+]
