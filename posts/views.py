1ff89c4e4df584a2df53d7355b0db8edf44cd930
Shahid Hasan Shuvo <mrshanshuvo@gmail.com>
2025-12-18 18:29:38 +0600

    learning the authentication

--- posts/views.py
+++ posts/views.py
@@ -1,19 +1,15 @@
 from django.shortcuts import render
 from django.http import Http404
 from .models import Post
-from django.shortcuts import get_list_or_404
+from django.shortcuts import get_object_or_404
+
 
 # Create your views here.
 def home(request):
-  all_posts = Post.objects.all()
-  return render(request, 'posts/index.html', {'posts' : all_posts})
+    all_posts = Post.objects.all()
+    return render(request, "posts/index.html", {"posts": all_posts})
+
 
 def post(request, id):
-  post = get_list_or_404(Post,id=id)
-  return render(request, 'posts/post.html', {'post': post})
-
-  
-  
-
-  
-  
+    post = get_object_or_404(Post, id=id)
+    return render(request, "posts/post.html", {"post": post})
