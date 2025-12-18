102e95416c2659171eff9a3963126adc70897937
Shahid Hasan Shuvo <mrshanshuvo@gmail.com>
2025-12-18 19:52:30 +0600

    learnt the register and login user

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,4 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("register/", views.register)]
+urlpatterns = [path("register/", views.register), path("login/", views.auth_login)]
--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from .forms import RegisterForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login
 
 
 def register(request):
@@ -14,3 +16,18 @@ def register(request):
         form = RegisterForm()
 
     return render(request, "accounts/register.html", {"form": form})
+
+
+def auth_login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                homeURL = reverse("home")
+                login(request, user)
+                return HttpResponseRedirect(homeURL)
+    form = AuthenticationForm()
+    return render(request, "accounts/login.html", {"form": form})
