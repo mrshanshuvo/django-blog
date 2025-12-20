5853dc05bd0226e42b25e06ed38c6b65e24d7a26
Shahid Hasan Shuvo <mrshanshuvo@gmail.com>
2025-12-21 02:17:53 +0600

    learnt pagination

--- posts/views.py
+++ posts/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from .models import Post
 from django.shortcuts import get_object_or_404
+from django.core.paginator import Paginator
 
 
 # Create your views here.
 def home(request):
-    all_posts = Post.objects.all()
-    return render(request, "posts/index.html", {"posts": all_posts})
+    all_posts = Post.objects.all().order_by("-id")
+    paginator = Paginator(all_posts, 4)
+    page_number = request.GET.get("p", 1)
+    page_obj = paginator.get_page(page_number)
+    return render(request, "posts/index.html", {"posts": page_obj})
 
 
 def post(request, id):
