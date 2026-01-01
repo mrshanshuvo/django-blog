b723e057215abdf52eb210d307dece907e0425c6
Shahid Hasan Shuvo <mrshanshuvo@gmail.com>
2026-01-01 19:23:46 +0600

    apply cbv

--- posts/views.py
+++ posts/views.py
@@ -6,39 +6,71 @@ from .forms import CommentForm
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.db.models import Q
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 
 # Create your views here.
-def home(request):
-    all_posts = Post.objects.all().order_by("-id")
-    paginator = Paginator(all_posts, 4)
-    page_number = request.GET.get("p", 1)
-    page_obj = paginator.get_page(page_number)
-    return render(
-        request,
-        "posts/index.html",
-        {"posts": page_obj, "page_number": all_posts.count()},
-    )
+# def home(request):
+#     all_posts = Post.objects.all().order_by("-id")
+#     paginator = Paginator(all_posts, 4)
+#     page_number = request.GET.get("p", 1)
+#     page_obj = paginator.get_page(page_number)
+#     return render(
+#         request,
+#         "posts/index.html",
+#         {"posts": page_obj, "page_number": all_posts.count()},
+#     )
 
 
-def post(request, id):
-    post = get_object_or_404(Post, id=id)
-    if request.method == "POST":
+class HomeView(ListView):
+    model = Post
+    template_name = "posts/index.html"
+    context_object_name = "posts"
+    ordering = ["-id"]
+    paginate_by = 4
+
+
+# def post(request, id):
+#     post = get_object_or_404(Post, id=id)
+#     if request.method == "POST":
+#         form = CommentForm(request.POST)
+#         if form.is_valid():
+#             comment = form.save(commit=False)
+#             comment.post = post
+#             comment.user = request.user
+#             comment.save()
+#             postURL = reverse("post", args=[id])
+#             return HttpResponseRedirect(postURL)
+
+#     form = CommentForm()
+#     return render(
+#         request,
+#         "posts/post.html",
+#         {"post": post, "form": form, "comments": post.comment_set.all()},
+#     )
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "posts/post.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form"] = CommentForm()
+        context["comments"] = self.object.comment_set.all()
+        return context
+
+    def post(self, request, pk):
+        self.object = self.get_object()
         form = CommentForm(request.POST)
         if form.is_valid():
             comment = form.save(commit=False)
-            comment.post = post
+            comment.post = self.object
             comment.user = request.user
             comment.save()
-            postURL = reverse("post", args=[id])
+            postURL = reverse("post", args=[pk])
             return HttpResponseRedirect(postURL)
-
-    form = CommentForm()
-    return render(
-        request,
-        "posts/post.html",
-        {"post": post, "form": form, "comments": post.comment_set.all()},
-    )
 
 
 def tags(request, id):
@@ -46,16 +78,39 @@ def tags(request, id):
     return render(request, "posts/tags.html", {"tags": tag.post_set.all()})
 
 
-def search(request):
-    q = request.GET.get("q", None)
-    page_number = request.GET.get("p", 1)
-    posts = Post.objects.filter(
-        Q(post_title__icontains=q) | Q(post_content__icontains=q)
-    ).order_by("-id")
-    paginator = Paginator(posts, 4)
-    page_obj = paginator.get_page(page_number)
-    return render(
-        request,
-        "posts/search.html",
-        {"posts": page_obj, "q": q, "post_number": posts.count()},
-    )
+# def search(request):
+#     q = request.GET.get("q", None)
+#     page_number = request.GET.get("p", 1)
+#     posts = Post.objects.filter(
+#         Q(post_title__icontains=q) | Q(post_content__icontains=q)
+#     ).order_by("-id")
+#     paginator = Paginator(posts, 4)
+#     page_obj = paginator.get_page(page_number)
+#     return render(
+#         request,
+#         "posts/search.html",
+#         {"posts": page_obj, "q": q, "post_number": posts.count()},
+#     )
+
+
+class SearchView(ListView):
+    model = Post
+    template_name = "posts/search.html"
+    context_object_name = "posts"
+    paginate_by = 4
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["query"] = self.request.GET.get("query")
+        return context
+
+    def get_queryset(self):
+        query = self.request.GET.get("query", "").strip()
+
+        if not query:
+            return Post.objects.none()
+
+        posts = Post.objects.filter(
+            Q(post_title__icontains=query) | Q(post_content__icontains=query)
+        ).order_by("-id")
+        return posts
